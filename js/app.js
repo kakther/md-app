8ca96883f315a72ce0c35ae93a317322833a988c
Kausar Akther <kausar.akther@gmail.com>
2021-04-12 20:09:11 -0400

    connect api

--- js/app.js
+++ js/app.js
@@ -1,13 +1,38 @@
 $(() => {
+    
+    const apikey = '5a03a681'
+    $('form').on('submit', (event) => {
+        // preventDefult will prevent auto submittion
+        event.preventDefault();
+        const $userInput = $('input[type="text"]').val();
 
-    $.ajax({
-        url: 'http://www.omdbapi.com/?i=tt3896198&apikey=5a03a681'
-    }).then(
-        (data) => {
-            console.log(data)
-        },
-        () => {
-            console.log('bad request')
-        }
-    );
+        $.ajax({
+
+            url: `http://www.omdbapi.com/?s=${$userInput}&apikey=5a03a681`
+        }).then(
+            (data) => {
+               
+                for(let i = 0; i < 10; i++) {
+
+                    // const $plot = $('<div>').addClass('movieInfo');
+                    const $poster = $('<img>').addClass('posterImg').attr('src', data.Search[i].Poster);
+                    const $title = $('<h1>');
+                    const $year = $('<h3>').addClass('year');
+                    const $runtime = $('<h3>').addClass('runtime');
+
+                   $('.row').append($poster)
+                
+                // $('.cardholder').append($movie_info)
+              
+
+                }
+                console.log(data)
+
+            },
+            () => {
+                console.log('bad request')
+            }
+        );
+    })
+    
 })
